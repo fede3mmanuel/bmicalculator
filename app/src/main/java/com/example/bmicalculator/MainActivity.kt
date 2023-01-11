76b0748c5fb1f6b8c7ae119c07b38ac2bac1ed00
Emmanuel Sanchez <fede@fede.website>
2023-01-10 18:36:26 -0600

    generate the bmi and showed on the app

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btnCalculate)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
+            // get result with two decimal places
+            val bmi2Digit = String.format("%.2f", bmi).toFloat()
+
+            displayResult(bmi2Digit)
+        }
+    }
+
+    private fun displayResult(bmi:Float) {
+        val resultIndex = findViewById<TextView>(R.id.tvIndex)
+        val resultDescription = findViewById<TextView>(R.id.tvResult)
+        val info = findViewById<TextView>(R.id.tvInfo)
+
+        resultIndex.text = bmi.toString()
+        info.text = "(Normal range is 18.5 - 24.9)"
+
     }
 }
